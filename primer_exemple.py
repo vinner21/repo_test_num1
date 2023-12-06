f709f110765ea57574fbe20a0b6635a1cadb49bc
Ferran Vidal <vidalmarginet@gmail.com>
2023-12-06 17:00:25 +0100

    Segona pujada d'exemple

---
+++ primer_exemple.py
@@ -0,0 +1,12 @@
+# Aquest ésun primer exemple de codi Python
+import pandas as pd
+
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+#load data into a DataFrame object:
+df = pd.DataFrame(data)
+
+print(df) 
